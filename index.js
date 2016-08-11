63aef1ad25ce16f5ee3b5e47939cb4ba5b8deaff
pavezzo <paavo.natti@gmail.com>
2016-08-11 20:29:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+app.get('/api/whoami', function(req, res) {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'].split(",")[0],
+    software: req.headers['user-agent'].split(/\((.*?)\)/)[1]
+  });
+  console.log(req.headers);
+});
+
+app.listen(8000);
